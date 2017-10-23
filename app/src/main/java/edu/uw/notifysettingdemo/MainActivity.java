0b434aa4932451882a2db0335312296e100aae9a
Wuyi Zhang <wuyi_zhang@outlook.com>
2017-10-23 14:21:40 -0700

    Simple Alert

--- app/src/main/java/edu/uw/notifysettingdemo/MainActivity.java
+++ app/src/main/java/edu/uw/notifysettingdemo/MainActivity.java
@@ -1,13 +1,17 @@
 package edu.uw.notifysettingdemo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,8 +38,31 @@ public class MainActivity extends AppCompatActivity {
     public void showAlert(View v){
         Log.d(TAG, "Alert button pressed");
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!");
+        builder.setMessage("Danger Will Robinson!");
+        builder.setPositiveButton("I see it!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "You cliked okay.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("AHHHHHHH", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "AHHHHHHHHHHHH", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
+    public static class AlertDialogFragment extends DialogFragment {
+
+        public 
+    }
 
     /* Menu handling */
 
